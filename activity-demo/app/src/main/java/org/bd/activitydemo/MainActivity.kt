dce9f76e6b63c27ca853f346e01a3270cbe9cba7
Philipp Auersperg-Castell <phil@bluedynamics.com>
2020-03-22 01:15:22 +0100

    add aar

--- activity-demo/app/src/main/java/org/bd/activitydemo/MainActivity.kt
+++ activity-demo/app/src/main/java/org/bd/activitydemo/MainActivity.kt
@@ -2,11 +2,61 @@ package org.bd.activitydemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import io.ktor.client.HttpClient
+import io.ktor.client.features.websocket.WebSockets
+import io.ktor.client.features.websocket.ws
+import io.ktor.http.HttpMethod
+import io.ktor.http.cio.websocket.Frame
+import io.ktor.http.cio.websocket.readBytes
+import io.ktor.http.cio.websocket.readText
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.android.synthetic.main.activity_main.*
+import org.kivy.kivynativeactivity.ServiceWebsockets
 
 class MainActivity : AppCompatActivity() {
+
+    val client = HttpClient {
+        install(WebSockets)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun testConnection(ip: String, port: Int, message:String = "Howdii") {
+        runBlocking {
+            client.ws(
+                    method = HttpMethod.Get,
+                    host = ip,
+                    port = port, path = "/"
+            ) {
+                send(Frame.Text(message))
+                // Receive frame.
+                val frame = incoming.receive()
+                when (frame) {
+                    is Frame.Text -> txtOutput.text=frame.readText()
+                    is Frame.Binary -> println(frame.readBytes())
+                }
+            }
+        }
+    }
+
+    fun onSend(v: View){
+
+        GlobalScope.launch(Dispatchers.IO) {
+            testConnection("localhost", 8077, editMessage.text.toString())
+        }
+    }
+
+    fun onStart(v: View) {
+        val context = applicationContext
+        ServiceWebsockets.prepare(context)
+        ServiceWebsockets.start(context, "")
+        btnSend.setEnabled(true)
     }
 }
